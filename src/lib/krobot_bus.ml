0f53eb4d23cb92b040238ed20248e383323ed143
Jérémie Dimino <jeremie@dimino.org>
2011-03-17 10:06:01 +0100

    [info] use the environment variable DBUS_KROBOT_BUS_ADDRESS to get the address of the krobot bus

--- src/lib/krobot_bus.ml
+++ src/lib/krobot_bus.ml
@@ -7,11 +7,26 @@
  * This file is a part of [kro]bot.
  *)
 
+let section = Lwt_log.Section.make "bus"
+
 type t = OBus_bus.t
 
 external of_bus : OBus_bus.t -> t = "%identity"
 external to_bus : t -> OBus_bus.t = "%identity"
 
-let bus = lazy(OBus_bus.of_addresses [OBus_address.make "unix" [("abstract", "krobot")]])
+let bus = lazy(
+  let addresses =
+    match try Some(Sys.getenv "DBUS_KROBOT_BUS_ADDRESS") with Not_found -> None with
+      | Some addr ->
+          OBus_address.of_string addr
+      | None ->
+          [OBus_address.make "unix" [("abstract", "krobot")]]
+  in
+  try_lwt
+    OBus_bus.of_addresses addresses
+  with exn ->
+    lwt () = Lwt_log.error_f ~section ~exn "failed to connect to the krobot bus at address '%s'" (OBus_address.to_string addresses) in
+    raise_lwt exn
+)
 
 let get () = Lazy.force bus
