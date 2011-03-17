06f798b16d8539b5b2e6c688a6016f05a3974318
Jérémie Dimino <jeremie@dimino.org>
2011-03-17 18:49:59 +0100

    [info] make the driver to never fail

--- src/driver/krobot_driver.ml
+++ src/driver/krobot_driver.ml
@@ -19,13 +19,29 @@ let () = Krobot_init.arg "-device" (Arg.Set_string device) "<device> The device 
 lwt () =
   lwt bus = Krobot_init.init_service "Driver" in
 
-  (* Open the CAN bus. *)
-  lwt can = Krobot_can_bus.open_can !device in
-
-  (* D-Bus --> CAN *)
-  E.keep (E.map_s (Krobot_can_bus.send can) (Krobot_can.recv bus));
-
-  (* CAN --> D-Bus *)
   while_lwt true do
-    Krobot_can_bus.recv can >>= Krobot_can.send bus
+    try_lwt
+      (* Open the CAN bus. *)
+      lwt can = Krobot_can_bus.open_can !device in
+
+      let active, set_active = S.create true in
+
+      (* D-Bus --> CAN *)
+      let ev = E.map_s (Krobot_can_bus.send can) (E.when_ active (Krobot_can.recv bus)) in
+
+      try_lwt
+        (* CAN --> D-Bus *)
+        while_lwt true do
+          Krobot_can_bus.recv can >>= Krobot_can.send bus
+        done
+      with exn ->
+        (* Make sure no more messages are sent on the CAN bus. *)
+        set_active false;
+        (* This is just here to keep a reference to [ev]. *)
+        E.stop ev;
+        raise_lwt exn
+    with exn ->
+      lwt () = Lwt_log.error ~exn "failure" in
+      (* Wait a bit before retrying. *)
+      Lwt_unix.sleep 0.5
   done
