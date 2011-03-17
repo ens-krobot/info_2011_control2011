7ecf9fd338fcb4455f0195d376e8861be3af59b9
Jérémie Dimino <jeremie@dimino.org>
2011-03-17 03:14:13 +0100

    [info] add krobot-dump
    It prints all CAN frames that passes on D-Bus.

---
+++ src/tools/krobot_dump.ml
@@ -0,0 +1,24 @@
+(*
+ * krobot_dump.ml
+ * --------------
+ * Copyright : (c) 2011, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of [kro]bot.
+ *)
+
+(* Print CAN frames that passes on D-Bus. *)
+
+open Lwt
+open Lwt_react
+
+lwt () =
+  lwt bus = Krobot_bus.get () in
+
+  E.keep
+    (E.map_s
+       (fun msg ->
+          Lwt_io.printl (Krobot_message.to_string msg))
+       (Krobot_message.recv bus));
+
+  fst (wait ())
