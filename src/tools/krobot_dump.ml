65704a9e9094e0b0330c6fa92629646d4b2d0192
Jérémie Dimino <jeremie@dimino.org>
2011-03-29 03:06:22 +0200

    [info] allow to prints raw messages in krobot-dump

--- src/tools/krobot_dump.ml
+++ src/tools/krobot_dump.ml
@@ -12,13 +12,41 @@
 open Lwt
 open Lwt_react
 
+let raw = ref false
+let decoded = ref true
+
+let options = Arg.align [
+  "-raw", Arg.Set raw, " prints raw CAN frames";
+  "-no-decoded", Arg.Clear decoded, " do not prints decoded frames";
+]
+
+let usage = "\
+Usage: krobot-dump [options]
+options are:"
+
 lwt () =
+  Arg.parse options ignore usage;
+
   lwt bus = Krobot_bus.get () in
 
   E.keep
     (E.map_s
-       (fun (timestamp, msg) ->
-          Lwt_io.printlf "%f, %s" timestamp (Krobot_message.to_string msg))
-       (Krobot_message.recv bus));
+       (fun (timestamp, frame) ->
+          let msg = Krobot_message.decode frame in
+          lwt () = Lwt_io.printf "%f" timestamp in
+          lwt () =
+            if !decoded then
+              Lwt_io.printf ", %s" (Krobot_message.to_string msg)
+            else
+              return ()
+          in
+          lwt () =
+            if !raw then
+              Lwt_io.printf ", %s" (Krobot_can.string_of_frame frame)
+            else
+              return ()
+          in
+          Lwt_io.printl "")
+       (Krobot_can.recv bus));
 
   fst (wait ())
