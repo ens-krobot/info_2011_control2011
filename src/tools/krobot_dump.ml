7e5f2ef4666b0060e036df1520566ba99f057a2b
Jérémie Dimino <jeremie@dimino.org>
2011-03-31 00:05:32 +0200

    [krobot_dump] display better timestamps

--- src/tools/krobot_dump.ml
+++ src/tools/krobot_dump.ml
@@ -24,6 +24,33 @@ let usage = "\
 Usage: krobot-dump [options]
 options are:"
 
+let date_string time =
+  let tm = Unix.localtime time in
+  let month_string =
+    match tm.Unix.tm_mon with
+      | 0 -> "Jan"
+      | 1 -> "Feb"
+      | 2 -> "Mar"
+      | 3 -> "Apr"
+      | 4 -> "May"
+      | 5 -> "Jun"
+      | 6 -> "Jul"
+      | 7 -> "Aug"
+      | 8 -> "Sep"
+      | 9 -> "Oct"
+      | 10 -> "Nov"
+      | 11 -> "Dec"
+      | _ -> Printf.ksprintf failwith "Lwt_log.ascdate: invalid month, %d" tm.Unix.tm_mon
+  in
+  Printf.sprintf
+    "%s %2d %02d:%02d:%02d.%s"
+    month_string
+    tm.Unix.tm_mday
+    tm.Unix.tm_hour
+    tm.Unix.tm_min
+    tm.Unix.tm_sec
+    (String.sub (Printf.sprintf "%.4f" (fst (modf time))) 2 4)
+
 lwt () =
   Arg.parse options ignore usage;
 
@@ -33,16 +60,16 @@ lwt () =
     (E.map_s
        (fun (timestamp, frame) ->
           let msg = Krobot_message.decode frame in
-          lwt () = Lwt_io.printf "%f" timestamp in
+          lwt () = Lwt_io.print (date_string timestamp)in
           lwt () =
             if !decoded then
-              Lwt_io.printf ", %s" (Krobot_message.to_string msg)
+              Lwt_io.printf ": %s" (Krobot_message.to_string msg)
             else
               return ()
           in
           lwt () =
             if !raw then
-              Lwt_io.printf ", %s" (Krobot_can.string_of_frame frame)
+              Lwt_io.printf ": %s" (Krobot_can.string_of_frame frame)
             else
               return ()
           in
