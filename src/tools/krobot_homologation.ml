019fc84e07cd0bcd80ab312c4be04f47450c8c24
Pierre Chambart <pierre.chambart@ocamlpro.org>
2013-05-09 11:45:12 +0200

    homo

--- src/tools/krobot_homologation.ml
+++ src/tools/krobot_homologation.ml
@@ -166,6 +166,55 @@ let strat_base status =
     End;
   ]
 
+let { y = init_y },_ = Krobot_config.red_initial_position
+
+
+let linear a0 an n =
+  let dx = (an -. a0) /. (float (n-1)) in
+  let p n = a0 +. (float n) *. dx in
+  Array.init n p
+
+let stupid_path init_x =
+  let l = linear init_x 1.5 10 in
+  Array.to_list (Array.map (fun x -> {x; y = init_y }) l)
+
+let stupid_path = function
+  | `Red -> [{ x = 2.3; y = init_y }]
+  | `Blue -> [{ x = 0.7; y = init_y }]
+
+let godx = function
+  | `Red -> 1.
+  | `Blue -> -.1.
+
+let strat_base status =
+  let ({x = init_x},_) = match status.team with
+    | `Red -> Krobot_config.red_initial_position
+    | `Blue -> Krobot_config.blue_initial_position in
+  (* let path = stupid_path init_x in *)
+  let path = stupid_path status.team in
+  Printf.printf "update strat\n%!";
+  [
+    Stop_timer;
+    Wait_for 0.1;
+    Reset_odometry `Auto;
+    Can (Krobot_message.encode (Drive_activation true));
+    Wait_for_jack true;
+    Can (Krobot_message.encode (Ax12_Set_Torque_Enable (2,true)));
+    Can (Krobot_message.encode (Ax12_Goto (2, ax12_2_base_position, 100)));
+    Can (Krobot_message.encode (Ax12_Goto (1, ax12_1_base_position, 100)));
+    Wait_for 1.;
+    Wait_for_jack false;
+    Start_timer (90.,[Stop] @ gonfle_baloon @ [End]);
+    Set_led(`Red,false);
+    Set_led(`Green,false);
+    Reset_odometry `Auto;
+    Wait_for_odometry_reset `Auto;
+    Set_limits (vmax,omega_max,accel_tan_max,accel_rad_max);
+    Stop;
+    Follow_path (path, Some { vx = godx status.team; vy = 0. }, false);
+    Stop;
+    End;
+  ]
 
 let launch bus status =
   Krobot_bus.send bus
